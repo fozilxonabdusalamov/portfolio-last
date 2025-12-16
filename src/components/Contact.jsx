9640aecde3d1c90309deba274eb4c598b4794bf4
fozilxonabdusalamov <fozilxonabdusalamov2006@gmail.com>
2025-12-16 11:24:41 -0800

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,76 @@
+import { MdEmail } from "react-icons/md";
 import "../style/Contact.css";
+import { IoMdMap } from "react-icons/io";
+import { IoIosPhonePortrait } from "react-icons/io";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div>
+        <form action="">
+          <label htmlFor="fullname">
+            Your Full Name ( Required)
+            <input type="text" id="fullname" required />
+          </label>
+          <label htmlFor="email">
+            Your Email ( Required)
+            <input type="email" id="email" required />
+          </label>
+          <label htmlFor="subject">
+            Your Subject ( Required)
+            <input type="text" id="subject" required />
+          </label>
+          <label htmlFor="message">
+            Your Message ( Required)
+            <textarea id="message" required></textarea>
+          </label>
+          <button type="submit">Send Message</button>
+        </form>
+        <div>
+          <h2>Contact Information</h2>
+          <div>
+            <IoMdMap />
+            <p>
+              Country: <span>Uzbekistan</span>
+            </p>
+            <p>
+              City: <span>Namangan</span>
+            </p>
+            <p>
+              Street: <span>Yangi Hayot</span>
+            </p>
+          </div>
+          <div>
+            <MdEmail />
+            <p>
+              Email: <span>fozilxonabdusalamov2006@gmail.com</span>
+            </p>
+            <p>
+              Telegram: <span>@fozilxon25x</span>
+            </p>
+            <p>
+              Instagram: <span>fozilxon25x</span>
+            </p>
+          </div>
+          <div>
+            <IoIosPhonePortrait />
+            <p>
+              Support: <span>+998 90 123 45 67</span>
+            </p>
+            <p>
+              WhatsApp: <span>+998 90 123 45 67</span>
+            </p>
+            <p>
+              Phone: <span>+998 90 123 45 67</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        © {new Date().getFullYear()} Fozilxon Abdusalamov. All Rights Reserved.
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,16 @@
-import '../style/Main.css'
-import Banner from './Banner'
-import Services from './Services'
+import "../style/Main.css";
+import Banner from "./Banner";
+import Contact from "./Contact";
+import Services from "./Services";
 
 function Main() {
   return (
-    <div className='main_container'>
-
-        <Banner />
-        <Services />
+    <div className="main_container">
+      <Banner />
+      <Services />
+      <Contact />
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
